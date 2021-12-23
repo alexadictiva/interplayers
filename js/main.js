ed327ded0ffdfcf83639e6774cdb80d77c8ed227
Libia Perez <alexandra220790@gmail.com>
2021-12-23 15:07:06 -0300

    adding fonts and js functionality

--- js/main.js
+++ js/main.js
@@ -5,3 +5,17 @@ var swiper = new Swiper(".mySwiper", {
     disableOnInteraction: false,
   },
 });
+function showMore() {
+  let paragraph = document.querySelector(".aboutHidden");
+  paragraph.classList.toggle("hidden");
+}
+
+function showProfessional() {
+  let professional = document.querySelector(".professionalHidden");
+  professional.classList.toggle("hidden2");
+}
+
+function showYouth() {
+  let youth = document.querySelector(".youthHidden");
+  youth.classList.toggle("hidden2");
+}
