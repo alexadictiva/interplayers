b56bb8e2f9056e7d78041959fb93028da114d464
Libia Perez <alexandra220790@gmail.com>
2021-12-28 00:17:33 -0300

    adding btn functionalities

--- js/main.js
+++ js/main.js
@@ -9,13 +9,43 @@ function showMore() {
   let paragraph = document.querySelector(".aboutHidden");
   paragraph.classList.toggle("hidden");
 }
-
 function showProfessional() {
-  let professional = document.querySelector(".professionalHidden");
-  professional.classList.toggle("hidden2");
+  let professional5 = document.querySelector(".professional5");
+  let professional6 = document.querySelector(".professional6");
+  let professional7 = document.querySelector(".professional7");
+  let professional8 = document.querySelector(".professional8");
+  let professional9 = document.querySelector(".professional9");
+  let professional10 = document.querySelector(".professional10");
+  let professional11 = document.querySelector(".professional11");
+  let professional12 = document.querySelector(".professional12");
+  let professional13 = document.querySelector(".professional13");
+  let professional14 = document.querySelector(".professional14");
+  let professional15 = document.querySelector(".professional15");
+  let cuartoP = document.querySelector(".cuartoP");
+  professional5.classList.toggle("hidden");
+  professional6.classList.toggle("hidden");
+  professional7.classList.toggle("hidden");
+  professional8.classList.toggle("hidden");
+  professional9.classList.toggle("hidden");
+  professional10.classList.toggle("hidden");
+  professional11.classList.toggle("hidden");
+  professional12.classList.toggle("hidden");
+  professional13.classList.toggle("hidden");
+  professional14.classList.toggle("hidden");
+  professional15.classList.toggle("hidden");
+  cuartoP.classList.toggle("hiddenTablet");
 }
 
 function showYouth() {
-  let youth = document.querySelector(".youthHidden");
-  youth.classList.toggle("hidden2");
+  let youth5 = document.querySelector(".youth5");
+  let youth6 = document.querySelector(".youth6");
+  let youth7 = document.querySelector(".youth7");
+  let youth8 = document.querySelector(".youth8");
+  let cuartoY = document.querySelector(".cuartoY");
+  youth5.classList.toggle("hidden");
+  youth6.classList.toggle("hidden");
+  youth7.classList.toggle("hidden");
+  youth8.classList.toggle("hidden");
+
+  cuartoY.classList.toggle("hiddenTablet");
 }
